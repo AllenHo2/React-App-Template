dd897d6a605f1695df25a6133e07b85611af8309
AllenHo2 <aho22@dons.usfca.edu>
2024-01-20 12:32:48 -0800

    React Template

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,27 @@
+import github_icon from '../assets/github_icon.png';
+import linkedin_icon from '../assets/linkedin_icon.png';
+import email_icon from '../assets/email_icon.png';
+//import twitter_icon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        title: 'Github',
+        image: github_icon,
+        link: 'https://github.com/AllenHo2',
+    },
+    {
+        id: 2,
+        title: 'LinkedIn',
+        image: linkedin_icon,
+        link: 'https://www.linkedin.com/in/allen-ho-b67a6725b/',
+    },
+    {
+        id: 3,
+        title: 'Email',
+        image: email_icon,
+        link: 'mailto:allenho2242004@gmail.com'
+    }
+];
+
+export default SOCIAL_PROFILES;
